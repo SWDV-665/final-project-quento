97bc3f6329878c28d171dc2608f8539067d94dd7
Quentin O <qochieng@gmail.com>
2018-12-15 03:22:08 -0600

    Added basic crud to test logic.

--- src/providers/workout-service/workout-service.ts
+++ src/providers/workout-service/workout-service.ts
@@ -22,6 +22,11 @@ export class WorkoutServiceProvider {
         }>      
     }>;
 
+  exercises: Array<{
+      name:string,
+      desc: string
+    }>;
+
   constructor() {
     console.log('Hello WorkoutServiceProvider Provider');
     
@@ -61,6 +66,36 @@ export class WorkoutServiceProvider {
         ]      
       }
     ];
+
+    this.exercises = [
+      {
+        name: "Push Ups",
+        desc: "Place your hands on the floor so they’re slightly outside shoulder-width. Spread your fingers slightly out and have them pointed forward. Raise up onto your toes so that all of your body weight is on your hands and your feet.Bend your elbows and lower your chest down toward the floor. Once your elbows bend slightly beyond 90 degrees, push off the floor and extend them so that you return to starting position."
+      },
+      {
+        name: "Sit ups",
+        desc: "Lie flat on your back, lifts the torso to a sitting position, and then lie flat again without changing the position of the legs: formerly done with the legs straight but now usually done with the knees bent. "
+      },
+      {
+        name: "Squats",
+        desc: "From a standing position. Bend your knees keeping your back straight. Stop when your thights are perpendicular to the flow. Slowly rise back to standing position."
+      }
+    ];
   }
 
+  getWorkout(){
+    return this.workouts;
+  }
+
+  removeWorkout(index){
+    this.workouts.splice(index, 1);
+  }
+
+  addWorkout(workout){
+    this.workouts.push(workout);
+  }
+
+  editWorkout(workout, index){
+    this.workouts[index] = workout;
+  }
 }
