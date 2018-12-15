7db7377b5ba9058708d2cfe64ce6c22ddaf4fdb4
Quentin O <qochieng@gmail.com>
2018-12-14 21:33:16 -0600

    Added data to data service

--- src/providers/workout-service/workout-service.ts
+++ src/providers/workout-service/workout-service.ts
@@ -10,8 +10,57 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class WorkoutServiceProvider {
 
+  
+
+  workouts : Array<{
+      name: string,
+      sets: number,
+      description: string,
+      exercises: Array<{
+          name: string,
+          reps: number
+        }>      
+    }>;
+
   constructor() {
     console.log('Hello WorkoutServiceProvider Provider');
+    
+    this.workouts = [
+      {
+        name: "Work Out 1",
+        sets: 5,
+        description: "1st work out created.",
+        exercises: [
+          {
+            name: "Push ups",
+            reps: 5
+          },
+          {
+            name: "Sit ups",
+            reps: 3
+          },
+          {
+            name: "Jumping Jacks",
+            reps: 8
+          }
+        ]      
+      },
+      {
+        name: "Work Out 2",
+        sets: 2,
+        description: "This is the second work out. I think it will be more vigourous.",
+        exercises: [
+          {
+            name: "Squats",
+            reps: 3
+          },
+          {
+            name: "Crunches",
+            reps: 5
+          }
+        ]      
+      }
+    ];
   }
 
 }
