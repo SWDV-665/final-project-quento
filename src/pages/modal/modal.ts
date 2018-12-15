96ec4ca7346fcf544dd2b36a857b9501a7cf961e
Quentin O <qochieng@gmail.com>
2018-12-15 06:14:25 -0600

    Added toast controller  and dialop provider to modal window.
    Updated toast, dialog and showExercisePrompt()

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Platform, ViewController } from 'ionic-angular';
 import { WorkoutServiceProvider } from '../../providers/workout-service/workout-service';
-
+import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
+import { ToastController } from 'ionic-angular';
 /**
  * Generated class for the ModalPage page.
  *
@@ -16,13 +17,42 @@ import { WorkoutServiceProvider } from '../../providers/workout-service/workout-
 })
 export class ModalPage {
   workout;
-  constructor(public navCtrl: NavController, public params: NavParams, public dataService: WorkoutServiceProvider, public platform: Platform, public viewCtrl: ViewController) {
+  workoutIndex;
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public params: NavParams, public dataService: WorkoutServiceProvider, public platform: Platform, public viewCtrl: ViewController, public dialogService: InputDialogServiceProvider) {
     this.workout = this.params.get('workout');
+    this.workoutIndex = this.params.get('index');
     console.log("workout name => " + this.workout);
    
   }
+
   dismiss() {
       this.viewCtrl.dismiss();
+  }
+
+  addExercise(){
+    this.dialogService.showExercisePrompt(this.workoutIndex);
+  }
+
+  editExercise(exercise, index){
+    console.log("Edit  " + exercise.name + " index " + index + " workout index " + this.workoutIndex );
+    //this.dataService.workouts[this.workoutIndex].editExercise(exercise,index);
+    this.dialogService.showExercisePrompt(this.workoutIndex,exercise, index);
+  }
+
+  removeExercise(exercise, index){
+
+    console.log("delete " + exercise.name + " index " + index + " workout index " + this.workoutIndex );
+
+    // Show item removal message (toast)
+    const toast = this.toastCtrl.create({
+      message: 'Deleting - ' + exercise.name + " ...",
+      duration: 3000
+    });
+    toast.present();
+    
+    // Use Groceries Provider to remove item
+    this.dataService.workouts[this.workoutIndex].removeExercise(exercise, index);
+
   }
 
   ionViewDidLoad() {
