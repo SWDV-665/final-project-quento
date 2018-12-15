20b26a597bcd5b2b24e46175615b00d6fd25fd7e
Quentin O <qochieng@gmail.com>
2018-12-14 20:16:16 -0600

    workout-service added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { WorkoutServiceProvider } from '../providers/workout-service/workout-service';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    WorkoutServiceProvider
   ]
 })
 export class AppModule {}
