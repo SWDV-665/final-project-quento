a837d57c119bca236ed6fc84a9ea4ecca1a0a4da
Quentin O <qochieng@gmail.com>
2018-12-15 03:23:10 -0600

    Added reference to home page.

--- src/pages/modal/modal.module.ts
+++ src/pages/modal/modal.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ModalPage } from './modal';
+import { HomePage } from '../home/home';
 
 @NgModule({
   declarations: [
@@ -9,5 +10,8 @@ import { ModalPage } from './modal';
   imports: [
     IonicPageModule.forChild(ModalPage),
   ],
+  entryComponents: [
+    HomePage,
+  ]
 })
 export class ModalPageModule {}
