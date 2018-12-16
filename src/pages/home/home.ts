e4b5920157f6c12e859dcd45f35561b6bf0b6cef
Quentin O <qochieng@gmail.com>
2018-12-15 22:14:11 -0600

    Added social sharing and load change checker to constructor.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,7 @@ import { ModalController } from 'ionic-angular';
 import { WorkoutServiceProvider } from '../../providers/workout-service/workout-service';
 import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
 import { ModalPage } from '../modal/modal';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 @Component({
   selector: 'page-home',
@@ -14,16 +15,30 @@ import { ModalPage } from '../modal/modal';
 export class HomePage {
 
   title : string;
-
+  workouts = [];
   errorMessage: string;
 
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: WorkoutServiceProvider,public modalCtrl: ModalController, public dialogService: InputDialogServiceProvider) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController, public dataService: WorkoutServiceProvider,public modalCtrl: ModalController, public dialogService: InputDialogServiceProvider, public socialSharing: SocialSharing) {
     this.title = "Workout Route";
+
+    // Subscribing to observable to know when data changes.
+    dataService.dataChanged$.subscribe((dataChanged: boolean) => {
+      this.loadWorkouts();
+    });
     
   }
 
+  ionViewDidLoad(){
+    this.loadWorkouts();
+  }
+
+  // Get initial list of workouts from the provider when the app loads.
   loadWorkouts(){
-    return this.dataService.getWorkouts();
+    //return this.dataService.getWorkouts();
+    this.dataService.getWorkouts().subscribe(
+      workouts => this.workouts = workouts,
+      error => this.errorMessage = <any>error
+    );
   }
 
   removeWorkout(workout, index){
@@ -35,7 +50,7 @@ export class HomePage {
     toast.present();
 
     // Use Groceries Provider to remove item
-    this.dataService.removeWorkout(index);
+    this.dataService.removeWorkout(workout._id);
   }
 
   editWorkout(workout, index){
@@ -50,7 +65,7 @@ export class HomePage {
     toast.present();
 
     // Edit item from items array    
-    this.dialogService.showPrompt(workout, index);
+    this.dialogService.showPrompt(workout, index, workout._id);
   }
 
   addWorkout(){
