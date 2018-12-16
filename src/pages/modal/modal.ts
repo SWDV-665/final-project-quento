6ed1ca61b18895bec92c0f912a64563ff1c0909b
Quentin O <qochieng@gmail.com>
2018-12-15 19:19:46 -0600

    Added social sharing code, button and plugin.

--- src/pages/modal/modal.ts
+++ src/pages/modal/modal.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams, Platform, ViewController } from 'i
 import { WorkoutServiceProvider } from '../../providers/workout-service/workout-service';
 import { InputDialogServiceProvider } from '../../providers/input-dialog-service/input-dialog-service';
 import { ToastController } from 'ionic-angular';
+import { SocialSharing } from '@ionic-native/social-sharing';
 /**
  * Generated class for the ModalPage page.
  *
@@ -18,7 +19,7 @@ import { ToastController } from 'ionic-angular';
 export class ModalPage {
   workout;
   workoutIndex;
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public params: NavParams, public dataService: WorkoutServiceProvider, public platform: Platform, public viewCtrl: ViewController, public dialogService: InputDialogServiceProvider) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public params: NavParams, public dataService: WorkoutServiceProvider, public platform: Platform, public viewCtrl: ViewController, public dialogService: InputDialogServiceProvider, public socialSharing: SocialSharing) {
     this.workout = this.params.get('workout');
     this.workoutIndex = this.params.get('index');
     console.log("workout name => " + this.workout.name + " Workout Index => " + this.workoutIndex);
@@ -53,11 +54,38 @@ export class ModalPage {
     
     // Use Groceries Provider to remove item
     this.dataService.workouts[this.workoutIndex].removeExercise(exercise, index);
-
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ModalPage');
+  }
+
+  shareExercise(exercise, index){
+    console.log("Shared successfully");
+    let message = "Exercise - Name: " + exercise.name + " - Reps: " + exercise.reps;
+    let subject = "Shared via WorkoutRoute app";
+    // Share an item    
+    this.socialSharing.share(message,subject).then(() => {
+      console.log("Shared successfully");
+      // Show action notification
+      this.showToast("Shared successfully", exercise, index);
+    }).catch((error) => {
+      console.error("Error while sharing ", error);
+      // Show action notification
+      this.showToast("Share Error!", exercise, index);
+    });
+  }
+
+  showToast(itemAction, itemObj, id){
+    // Show a toast message to Troubleshoot action occurrance.
+    console.log(itemAction + " item .." , id, itemObj._id);
+    
+    // Show item action message (toast)
+    const toast = this.toastCtrl.create({
+      message: itemAction + ' item - ' + itemObj.name + " ...",
+      duration: 3000
+    });
+    toast.present();
   }
 
 }
