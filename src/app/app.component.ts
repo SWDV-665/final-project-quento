08eb1ffbcbd492811eb57a3ed36d033327897fe2
QuentO <qochieng@gmail.com>
2019-06-02 23:33:52 -0700

    Update app.component.ts
    Added homepage and googlePlus pluging. Created checkLogin() functions.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { GooglePlus } from '@ionic-native/google-plus';
 
 import { WelcomePage } from '../pages/welcome/welcome';
+import { HomePage } from '../pages/home/home';
+
 
 @Component({
   templateUrl: 'app.html'
@@ -11,12 +14,27 @@ import { WelcomePage } from '../pages/welcome/welcome';
 export class MyApp {
   rootPage:any = WelcomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public googlePlus: GooglePlus, public zone: NgZone) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();      
+      splashScreen.hide();
+      this.zone.run(() => {
+        this.checkLogin();
+      });      
     });
   }
+  async checkLogin(){
+    try {
+      let status = await this.googlePlus.trySilentLogin({});
+      console.log("User Found");
+      this.rootPage = WelcomePage;
+    } catch (error) {
+      console.log("User Not Found");
+      this.rootPage = HomePage;
+    }
+  }
+  
 }
+
